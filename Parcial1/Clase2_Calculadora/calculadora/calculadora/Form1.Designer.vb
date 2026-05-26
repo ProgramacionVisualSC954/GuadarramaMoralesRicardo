0ec01e84ee05e12ba7dfe4417d96459877d16d78
RICKAR2GAMA1 <107510983+RICKAR2GAMA1@users.noreply.github.com>
2026-05-25 23:00:20 -0600

    version final

---
+++ Parcial1/Clase2_Calculadora/calculadora/calculadora/Form1.Designer.vb
@@ -0,0 +1,326 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        resultado = New TextBox()
+        proceso = New TextBox()
+        igual = New Button()
+        del = New Button()
+        dividir = New Button()
+        porcentaje = New Button()
+        multiplicar = New Button()
+        restar = New Button()
+        sumar = New Button()
+        potencia = New Button()
+        raiz = New Button()
+        parentesis = New Button()
+        punto = New Button()
+        tres = New Button()
+        seis = New Button()
+        nueve = New Button()
+        uno = New Button()
+        pos_o_neg = New Button()
+        ocho = New Button()
+        dos = New Button()
+        cero = New Button()
+        cinco = New Button()
+        cuatro = New Button()
+        siete = New Button()
+        SuspendLayout()
+        ' 
+        ' resultado
+        ' 
+        resultado.BackColor = Color.White
+        resultado.Location = New Point(71, 39)
+        resultado.Multiline = True
+        resultado.Name = "resultado"
+        resultado.Size = New Size(563, 81)
+        resultado.TabIndex = 0
+        ' 
+        ' proceso
+        ' 
+        proceso.Location = New Point(71, 141)
+        proceso.Multiline = True
+        proceso.Name = "proceso"
+        proceso.Size = New Size(563, 83)
+        proceso.TabIndex = 1
+        ' 
+        ' igual
+        ' 
+        igual.Location = New Point(71, 515)
+        igual.Name = "igual"
+        igual.Size = New Size(94, 29)
+        igual.TabIndex = 2
+        igual.Text = "="
+        igual.UseVisualStyleBackColor = True
+        ' 
+        ' del
+        ' 
+        del.Location = New Point(71, 270)
+        del.Name = "del"
+        del.Size = New Size(94, 29)
+        del.TabIndex = 3
+        del.Text = "DEL"
+        del.UseVisualStyleBackColor = True
+        ' 
+        ' dividir
+        ' 
+        dividir.Location = New Point(186, 270)
+        dividir.Name = "dividir"
+        dividir.Size = New Size(94, 29)
+        dividir.TabIndex = 4
+        dividir.Text = "/"
+        dividir.UseVisualStyleBackColor = True
+        ' 
+        ' porcentaje
+        ' 
+        porcentaje.Location = New Point(419, 270)
+        porcentaje.Name = "porcentaje"
+        porcentaje.Size = New Size(94, 29)
+        porcentaje.TabIndex = 5
+        porcentaje.Text = "%"
+        porcentaje.UseVisualStyleBackColor = True
+        ' 
+        ' multiplicar
+        ' 
+        multiplicar.Location = New Point(540, 270)
+        multiplicar.Name = "multiplicar"
+        multiplicar.Size = New Size(94, 29)
+        multiplicar.TabIndex = 6
+        multiplicar.Text = "X"
+        multiplicar.UseVisualStyleBackColor = True
+        ' 
+        ' restar
+        ' 
+        restar.Location = New Point(540, 323)
+        restar.Name = "restar"
+        restar.Size = New Size(94, 29)
+        restar.TabIndex = 7
+        restar.Text = "-"
+        restar.UseVisualStyleBackColor = True
+        ' 
+        ' sumar
+        ' 
+        sumar.Location = New Point(540, 385)
+        sumar.Name = "sumar"
+        sumar.Size = New Size(94, 29)
+        sumar.TabIndex = 8
+        sumar.Text = "+"
+        sumar.UseVisualStyleBackColor = True
+        ' 
+        ' potencia
+        ' 
+        potencia.Location = New Point(302, 270)
+        potencia.Name = "potencia"
+        potencia.Size = New Size(94, 29)
+        potencia.TabIndex = 9
+        potencia.Text = "x²"
+        potencia.UseVisualStyleBackColor = True
+        ' 
+        ' raiz
+        ' 
+        raiz.Location = New Point(540, 448)
+        raiz.Name = "raiz"
+        raiz.Size = New Size(94, 29)
+        raiz.TabIndex = 10
+        raiz.Text = "√x"
+        raiz.UseVisualStyleBackColor = True
+        ' 
+        ' parentesis
+        ' 
+        parentesis.Location = New Point(540, 515)
+        parentesis.Name = "parentesis"
+        parentesis.Size = New Size(94, 29)
+        parentesis.TabIndex = 11
+        parentesis.Text = "( )"
+        parentesis.UseVisualStyleBackColor = True
+        ' 
+        ' punto
+        ' 
+        punto.Location = New Point(419, 515)
+        punto.Name = "punto"
+        punto.Size = New Size(94, 29)
+        punto.TabIndex = 12
+        punto.Text = "."
+        punto.UseVisualStyleBackColor = True
+        ' 
+        ' tres
+        ' 
+        tres.Location = New Point(419, 448)
+        tres.Name = "tres"
+        tres.Size = New Size(94, 29)
+        tres.TabIndex = 13
+        tres.Text = "3"
+        tres.UseVisualStyleBackColor = True
+        ' 
+        ' seis
+        ' 
+        seis.Location = New Point(419, 385)
+        seis.Name = "seis"
+        seis.Size = New Size(94, 29)
+        seis.TabIndex = 14
+        seis.Text = "6"
+        seis.UseVisualStyleBackColor = True
+        ' 
+        ' nueve
+        ' 
+        nueve.Location = New Point(419, 323)
+        nueve.Name = "nueve"
+        nueve.Size = New Size(94, 29)
+        nueve.TabIndex = 15
+        nueve.Text = "9"
+        nueve.UseVisualStyleBackColor = True
+        ' 
+        ' uno
+        ' 
+        uno.Location = New Point(186, 448)
+        uno.Name = "uno"
+        uno.Size = New Size(94, 29)
+        uno.TabIndex = 16
+        uno.Text = "1"
+        uno.UseVisualStyleBackColor = True
+        ' 
+        ' pos_o_neg
+        ' 
+        pos_o_neg.Location = New Point(186, 515)
+        pos_o_neg.Name = "pos_o_neg"
+        pos_o_neg.Size = New Size(94, 29)
+        pos_o_neg.TabIndex = 17
+        pos_o_neg.Text = "-/+"
+        pos_o_neg.UseVisualStyleBackColor = True
+        ' 
+        ' ocho
+        ' 
+        ocho.Location = New Point(302, 323)
+        ocho.Name = "ocho"
+        ocho.Size = New Size(94, 29)
+        ocho.TabIndex = 18
+        ocho.Text = "8"
+        ocho.UseVisualStyleBackColor = True
+        ' 
+        ' dos
+        ' 
+        dos.Location = New Point(302, 448)
+        dos.Name = "dos"
+        dos.Size = New Size(94, 29)
+        dos.TabIndex = 19
+        dos.Text = "2"
+        dos.UseVisualStyleBackColor = True
+        ' 
+        ' cero
+        ' 
+        cero.Location = New Point(302, 515)
+        cero.Name = "cero"
+        cero.Size = New Size(94, 29)
+        cero.TabIndex = 20
+        cero.Text = "0"
+        cero.UseVisualStyleBackColor = True
+        ' 
+        ' cinco
+        ' 
+        cinco.Location = New Point(302, 385)
+        cinco.Name = "cinco"
+        cinco.Size = New Size(94, 29)
+        cinco.TabIndex = 21
+        cinco.Text = "5"
+        cinco.UseVisualStyleBackColor = True
+        ' 
+        ' cuatro
+        ' 
+        cuatro.Location = New Point(186, 385)
+        cuatro.Name = "cuatro"
+        cuatro.Size = New Size(94, 29)
+        cuatro.TabIndex = 22
+        cuatro.Text = "4"
+        cuatro.UseVisualStyleBackColor = True
+        ' 
+        ' siete
+        ' 
+        siete.Location = New Point(186, 323)
+        siete.Name = "siete"
+        siete.Size = New Size(94, 29)
+        siete.TabIndex = 23
+        siete.Text = "7"
+        siete.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(691, 656)
+        Controls.Add(siete)
+        Controls.Add(cuatro)
+        Controls.Add(cinco)
+        Controls.Add(cero)
+        Controls.Add(dos)
+        Controls.Add(ocho)
+        Controls.Add(pos_o_neg)
+        Controls.Add(uno)
+        Controls.Add(nueve)
+        Controls.Add(seis)
+        Controls.Add(tres)
+        Controls.Add(punto)
+        Controls.Add(parentesis)
+        Controls.Add(raiz)
+        Controls.Add(potencia)
+        Controls.Add(sumar)
+        Controls.Add(restar)
+        Controls.Add(multiplicar)
+        Controls.Add(porcentaje)
+        Controls.Add(dividir)
+        Controls.Add(del)
+        Controls.Add(igual)
+        Controls.Add(proceso)
+        Controls.Add(resultado)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents resultado As TextBox
+    Friend WithEvents proceso As TextBox
+    Friend WithEvents igual As Button
+    Friend WithEvents del As Button
+    Friend WithEvents dividir As Button
+    Friend WithEvents porcentaje As Button
+    Friend WithEvents multiplicar As Button
+    Friend WithEvents restar As Button
+    Friend WithEvents sumar As Button
+    Friend WithEvents potencia As Button
+    Friend WithEvents raiz As Button
+    Friend WithEvents parentesis As Button
+    Friend WithEvents punto As Button
+    Friend WithEvents tres As Button
+    Friend WithEvents seis As Button
+    Friend WithEvents nueve As Button
+    Friend WithEvents uno As Button
+    Friend WithEvents pos_o_neg As Button
+    Friend WithEvents ocho As Button
+    Friend WithEvents dos As Button
+    Friend WithEvents cero As Button
+    Friend WithEvents cinco As Button
+    Friend WithEvents cuatro As Button
+    Friend WithEvents siete As Button
+
+End Class
---
+++ Parcial1/Clase2_Calculadora/calculadora/calculadora/Form1.vb
@@ -0,0 +1,254 @@
+﻿Option Strict On
+Imports System.Data
+
+Public Class Form1
+
+    '========================
+    ' AGREGAR TEXTO
+    '========================
+    Private Sub AgregarTexto(valor As String)
+
+        If valor = "(" Then
+
+            If proceso.Text.Length > 0 Then
+
+                Dim ultimo As Char = proceso.Text(proceso.Text.Length - 1)
+
+                If Char.IsDigit(ultimo) Then
+                    proceso.Text &= "*("
+                    Exit Sub
+                End If
+
+            End If
+
+        End If
+
+        proceso.Text &= valor
+
+    End Sub
+
+    '========================
+    ' NUMEROS
+    '========================
+    Private Sub cero_Click(sender As Object, e As EventArgs) Handles cero.Click
+        AgregarTexto("0")
+    End Sub
+
+    Private Sub uno_Click(sender As Object, e As EventArgs) Handles uno.Click
+        AgregarTexto("1")
+    End Sub
+
+    Private Sub dos_Click(sender As Object, e As EventArgs) Handles dos.Click
+        AgregarTexto("2")
+    End Sub
+
+    Private Sub tres_Click(sender As Object, e As EventArgs) Handles tres.Click
+        AgregarTexto("3")
+    End Sub
+
+    Private Sub cuatro_Click(sender As Object, e As EventArgs) Handles cuatro.Click
+        AgregarTexto("4")
+    End Sub
+
+    Private Sub cinco_Click(sender As Object, e As EventArgs) Handles cinco.Click
+        AgregarTexto("5")
+    End Sub
+
+    Private Sub seis_Click(sender As Object, e As EventArgs) Handles seis.Click
+        AgregarTexto("6")
+    End Sub
+
+    Private Sub siete_Click(sender As Object, e As EventArgs) Handles siete.Click
+        AgregarTexto("7")
+    End Sub
+
+    Private Sub ocho_Click(sender As Object, e As EventArgs) Handles ocho.Click
+        AgregarTexto("8")
+    End Sub
+
+    Private Sub nueve_Click(sender As Object, e As EventArgs) Handles nueve.Click
+        AgregarTexto("9")
+    End Sub
+
+    '========================
+    ' PUNTO
+    '========================
+    Private Sub punto_Click(sender As Object, e As EventArgs) Handles punto.Click
+        AgregarTexto(".")
+    End Sub
+
+    '========================
+    ' OPERADORES
+    '========================
+    Private Sub sumar_Click(sender As Object, e As EventArgs) Handles sumar.Click
+        AgregarTexto("+")
+    End Sub
+
+    Private Sub restar_Click(sender As Object, e As EventArgs) Handles restar.Click
+        AgregarTexto("-")
+    End Sub
+
+    Private Sub multiplicar_Click(sender As Object, e As EventArgs) Handles multiplicar.Click
+        AgregarTexto("*")
+    End Sub
+
+    Private Sub dividir_Click(sender As Object, e As EventArgs) Handles dividir.Click
+        AgregarTexto("/")
+    End Sub
+
+    '========================
+    ' PARENTESIS
+    '========================
+    Private Sub parentesis_Click(sender As Object, e As EventArgs) Handles parentesis.Click
+
+        If proceso.Text.Count(Function(c) c = "("c) >
+           proceso.Text.Count(Function(c) c = ")"c) Then
+
+            AgregarTexto(")")
+
+        Else
+            AgregarTexto("(")
+        End If
+
+    End Sub
+
+    '========================
+    ' IGUAL
+    '========================
+    Private Sub igual_Click(sender As Object, e As EventArgs) Handles igual.Click
+
+        Try
+
+            If proceso.Text.Trim = "" Then
+                MessageBox.Show("No hay operación")
+                Exit Sub
+            End If
+
+            'VALIDAR PARENTESIS
+            Dim abrir As Integer = proceso.Text.Count(Function(c) c = "("c)
+            Dim cerrar As Integer = proceso.Text.Count(Function(c) c = ")"c)
+
+            If abrir <> cerrar Then
+                MessageBox.Show("La expresión contiene paréntesis no balanceados")
+                Exit Sub
+            End If
+
+            Dim dt As New DataTable()
+
+            Dim res = dt.Compute(proceso.Text, "")
+
+            resultado.Text = Convert.ToDecimal(res).ToString("N2")
+
+        Catch ex As DivideByZeroException
+            MessageBox.Show("No se puede dividir entre cero")
+
+        Catch ex As OverflowException
+            MessageBox.Show("El resultado está fuera del rango permitido")
+
+        Catch ex As Exception
+            MessageBox.Show("Expresión inválida")
+        End Try
+
+    End Sub
+
+    '========================
+    ' RAIZ
+    '========================
+    Private Sub raiz_Click(sender As Object, e As EventArgs) Handles raiz.Click
+
+        Try
+
+            Dim numero As Decimal
+
+            If Decimal.TryParse(proceso.Text, numero) Then
+
+                If numero < 0 Then
+                    MessageBox.Show("La raíz cuadrada de un número negativo no es real")
+                    Exit Sub
+                End If
+
+                Dim res As Double = Math.Sqrt(CDbl(numero))
+
+                resultado.Text = res.ToString("N2")
+
+            Else
+                MessageBox.Show("Número inválido")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Error en la raíz")
+        End Try
+
+    End Sub
+
+    '========================
+    ' POTENCIA
+    '========================
+    Private Sub potencia_Click(sender As Object, e As EventArgs) Handles potencia.Click
+
+        Dim numero As Decimal
+
+        If Decimal.TryParse(proceso.Text, numero) Then
+
+            Dim res As Decimal = numero * numero
+
+            resultado.Text = res.ToString("N2")
+
+        Else
+            MessageBox.Show("Número inválido")
+        End If
+
+    End Sub
+
+    '========================
+    ' PORCENTAJE
+    '========================
+    Private Sub porcentaje_Click(sender As Object, e As EventArgs) Handles porcentaje.Click
+
+        Dim numero As Decimal
+
+        If Decimal.TryParse(proceso.Text, numero) Then
+
+            resultado.Text = (numero / 100D).ToString("N2")
+
+        Else
+            MessageBox.Show("Número inválido")
+        End If
+
+    End Sub
+
+    '========================
+    ' POSITIVO / NEGATIVO
+    '========================
+    Private Sub pos_o_neg_Click(sender As Object, e As EventArgs) Handles pos_o_neg.Click
+
+        Try
+
+            Dim numero As Decimal
+
+            If Decimal.TryParse(proceso.Text, numero) Then
+
+                numero *= -1
+
+                proceso.Text = numero.ToString()
+
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Error")
+        End Try
+
+    End Sub
+
+    '========================
+    ' BORRAR ULTIMO
+    '========================
+    Private Sub del_Click(sender As Object, e As EventArgs) Handles del.Click
+
+        If proceso.Text.Length > 0 Then
+            proceso.Text = proceso.Text.Substring(0, proceso.Text.Length - 1)
+        End If
+
+    End Sub
+
+End Class
